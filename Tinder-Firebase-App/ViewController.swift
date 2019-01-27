b95e2de7efb3cfd2ebb1caa627b8d2eb26c20192
BeaVDeveloper <yuravelkoa@gmail.com>
2019-01-27 11:13:21 +0200

    Initial Commit

---
+++ Tinder-Firebase-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinder-Firebase-App
+//
+//  Created by Yura Velko on 1/27/19.
+//  Copyright © 2019 Yura Velko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
