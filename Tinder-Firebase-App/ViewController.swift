c65736a5b9406678774364cdfed914d749dc9724
BeaVDeveloper <yuravelkoa@gmail.com>
2019-01-27 14:45:01 +0200

    Setup home view

--- Tinder-Firebase-App/ViewController.swift
+++ Tinder-Firebase-App/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let topStackView = TopNavigationStackView()
+    let cardsDeckView = UIView()
+    let buttonStackView = HomeBottomControlsStackView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        cardsDeckView.backgroundColor = .blue
+        setupLayout()
+        
+        setupCards()
+        
     }
 
+    fileprivate func setupCards() {
+        
+    }
+    
+    //MARK: - Fileprivate
+    
+    fileprivate func setupLayout() {
+        let overallStackView = UIStackView(arrangedSubviews: [topStackView, cardsDeckView, buttonStackView])
+        overallStackView.axis = .vertical
+        view.addSubview(overallStackView)
+        overallStackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
+    }
 
 }
 
---
+++ Tinder-Firebase-App/Views/HomeBottomControlsStackView.swift
@@ -0,0 +1,42 @@
+//
+//  HomeBottomControlsStackView.swift
+//  Tinder-Firebase-App
+//
+//  Created by Yura Velko on 1/27/19.
+//  Copyright © 2019 Yura Velko. All rights reserved.
+//
+
+import UIKit
+
+class HomeBottomControlsStackView: UIStackView {
+
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        distribution = .fillEqually
+        heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        let subviews = [#imageLiteral(resourceName: "refresh_circle"), #imageLiteral(resourceName: "dissmis_circle"), #imageLiteral(resourceName: "super_like_circle"), #imageLiteral(resourceName: "like_cricle"), #imageLiteral(resourceName: "boost_circle")].map { (img) -> UIView in
+            let button = UIButton(type: .system)
+            button.setImage(img.withRenderingMode(.alwaysOriginal), for: .normal)
+            return button
+        }
+        
+//        let button = UIButton(type: .system)
+//
+//        let bottomSubViews = [UIColor.red, .green, .blue, .yellow, .purple].map { (color) -> UIView in
+//            let v = UIView()
+//            v.backgroundColor = color
+//            return v
+//        }
+        
+        subviews.forEach({addArrangedSubview($0)})
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
